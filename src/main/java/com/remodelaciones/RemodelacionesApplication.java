d5cf6749bfa0bc184179fffad102b23741b9ffd4
mayronsb <seguramayron73@gmail.com>
2023-09-29 03:07:00 -0600

    primer commit

---
+++ src/main/java/com/remodelaciones/RemodelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.remodelaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemodelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemodelacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remodelaciones/RemodelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remodelaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemodelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
